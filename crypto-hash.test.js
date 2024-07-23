1328847ed0ad46cd725a1d6acd518d4998c7e856
TruongTran <truongtq1@batdongsan.com.vn>
2024-07-23 21:14:02 +0700

    Lession 13 Crypto Has and SHA-256

---
+++ crypto-hash.test.js
@@ -0,0 +1,18 @@
+const cryptoHash = require("./crypto-hash");
+
+// 2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
+
+//https://passwordsgenerator.net/sha256-hash-generator/
+describe("cryptoHash()", () => {
+  it("generates a SHA-256 hashed output", () => {
+    expect(cryptoHash("foo")).toEqual(
+      "2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae"
+    );
+  });
+
+  it("produces the same hash with the same input arguments in any order", () => {
+    expect(cryptoHash("one", "two", "three")).toEqual(
+      cryptoHash("three", "one", "two")
+    );
+  });
+});
